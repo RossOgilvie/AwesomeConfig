711284b1a02ac1c2b1d2b835e294b0cde2e87777
Ross Ogilvie <ross@rossogilvie.id.au>
2013-01-31 22:31:04 +1100

    change unfocused border to black

--- ross/theme/theme.lua
+++ ross/theme/theme.lua
@@ -29,7 +29,8 @@ theme.bg_urgent = "#3F3F3F"
 
 -- {{{ Borders
 theme.border_width  = "2"
-theme.border_normal = "#3F3F3F"
+--theme.border_normal = "#3F3F3F"
+theme.border_normal = "#00000000"
 theme.border_focus  = "#6F6F6F"
 theme.border_marked = "#CC9393"
 -- }}}
